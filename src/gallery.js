41663b2ddf857509c9e5d274f5f4206e975fa9c3
Doroshuk <dorosukolga100@gmail.com>
2021-05-28 23:11:07 +0300

    підключила імпорт на img

--- src/gallery.js
+++ src/gallery.js
@@ -1,2 +1,12 @@
-'use strict';
 import images from "./gallery-items.js";
+console.log(images);
+
+const refs = {
+    galleryEll: document.querySelector('.gallery'),
+    lightboxDivEll: document.querySelector('.lightbox'),
+    buttonCloseEll: document.querySelector('[data-action="close-lightbox"]'),
+lightboxOverlayEll: document.querySelector('.lightbox__overlay'),
+lightboxContentEll: document.querySelector('.lightbox__content'),
+}
+
+// console.log(refs);
